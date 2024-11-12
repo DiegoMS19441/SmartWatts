590018ce18f61b1ff9ea9dba74b9de7d2132b063
Diego.M <127765045+DiegoMS19441@users.noreply.github.com>
2024-11-11 22:27:24 -0300

    Iniciando o projeto no github

---
+++ src/main/java/br/com/fiap/smartwattts/SmartwatttsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.smartwattts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartwatttsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwatttsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/smartwattts/SmartwatttsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.smartwattts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwatttsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
